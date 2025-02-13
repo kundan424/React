415b6dc9d6bef2851dd5fc954aa5504aaa174c67
kundan kumar <kundankumar64355@gmail.com>
2025-02-13 22:56:27 +0530

    initial commit

---
+++ 07mega-blog/src/App.jsx
@@ -0,0 +1,14 @@
+
+import react from "react"
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+  return (
+    <>
+     <h1>hello </h1>
+    </>
+  )
+}
+
+export default App
